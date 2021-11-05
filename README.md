ed3bbd1b9ba0de49134cefab4b8c7b98f6f02f3a
Terukazu Nishimura <nishi@arrc.jp>
2021-11-05 14:30:14 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,140 @@
-# r2cv-ocv-t
+# Open CV4のインストールと概要(r2cv-ocv)
+OpenCV4のインストールとその概要を説明します。
+
+## 目次
+[1. Open CV4について](#1)
+
+[2. 画像処理](#2)
+
+[3.USBカメラの設定](#3)
+
+[<R2CVに戻る>](https://github.com/nishibra/r2cv)
+
+<a id="1"></a>
+## 1. Open CV4について
+### OpenCVとは
+OpenCVはオープンソースの画像処理や機械学習のソフトウェアーのライブラリーです。
+以下のサイトをご参照ください。
+
+>参考サイト:
+https://opencv.org/
+
+### インストールの手順
+#### SwapFile
+OpenCVをコンパイルするときに大量のメモリーを使いますので、メモリーが4Gbyte以下の場合はディスクをメモリーとして使うSwapfileをインストールします。
+```
+$ git clone https://github.com/JetsonHacksNano/installSwapfile
+$ cd installSwapfile
+$ ./installSwapfile.sh
+```
+#### opencv4のインストール
+Open cv4.*の最新版をインストールします、
+インストールスクリプトinstall-opencv.shをダウンロードして実行してください。
+```
+$ wget --no-check-certificate https://raw.githubusercontent.com/milq/milq/master/scripts/bash/install-opencv.sh
+$ chmod +x install-opencv.sh
+$ ./install-opencv.sh
+```
+コンパイルには数時間かかります。
+
+>参考サイト: http://milq.github.io/install-opencv-ubuntu-debian
+
+#### opencv-pythonのインストール
+```
+$ sudo apt install python3-pip
+$ sudo pip install opencv-python
+```
+#### OpenCVの動作確認
+USBカメラの接続
+Raspi4にUSBカメラを接続します。
+```
+$ ls /dev/video*
+```
+でカメラの接続を確認できます。
+
+#### カメラ画像読み込みプログラム
+まず$ homeディレクトリーにgitHubのairrcフォルダーのコピー作成します。cam_sample.pyを実行します。
+```
+$ cd airrc
+$ python3 cam_sample.py
+```
+と入力してください。画面にカメラの画像が表示されればPython3とOpenCVは動作しています。
+
+#### Open CVのサンプルプログラム
+以下のディレクトリーにPythonのサンプルプログラムがあります。画像処理に興味のある方はいろいろ使ってみましょう。
+```
+ubuntu@ubuntu:~/OpenCV/samples/python$
+  ~/OpenCV/samples/python$ python3 qrcode.py
+  ~/OpenCV/samples/python$ python3 video.py
+  ~/OpenCV/samples/python$ python3 houghlines.py
+  ~/OpenCV/samples/python$ python3 stereo_match.py
+```
+
+Deep learning関連
+```
+  ~/OpenCV/samples/dnn$
+```
+
+<a id="2"></a>
+## 2. 画像処理
+### 色認識
+・色認識　HSVが人の感覚と合うので色認識においては使用されます。探す色のHSV値にフィルターをかけます。次のプログラムはHSVのHueを数値化するプログラムです。
+USBカメラをセットし、プログラムのあるディレクトリーにて以下を実行してください。
+```
+$ cd airrc
+$ python3 hsv_color_set.py
+```
+![019](/pics-ocv/image019.png)
+
+画面にカメラ画像とHue Barが表示されます。緑の線の上をタッチするとHueの下限値、緑線の下をタッチすると上限をセットできます。
+その結果がカメラ画像に表示されます。ここで設定したHueの上下限を使えば色認識ができます。
+
+### ステレオカメラ
+ステレオカメラを使うと2つのカメラの視差から距離を求めることができます。
+視差を求めるにはステレオ画像マッチング手法を使います。
+マッチング手法にはSGM（Semi Global Matching）方式、SAD（Sum of Absolute Difference）方式などがありますが、変化のないところではマッチングできないため、赤外線や可視光でパターン照射を行っているものがあります。
+
+![020](/pics-ocv/image020.png)
+
+ここでは注目点を探してその点に対して視差から距離を求めます。
+サンプルプログラムでは注目点として赤色を認識してその距離を求めます。
+```
+$ cd airrc
+$ python3 color_stereo.py
+```
+![021](/pics-ocv/image021.png)
+
+このプログラムでは赤色のものまでの座標と距離を計算します。
+
+<a id="3"></a>
+## 3.USBカメラの設定
+### v4l2-ctl
+OpenCVからWebカメラを設定する方法としてv4l2-ctlを使う方法があります。
+>参考サイト: [v4l2-ctlで行うUSBカメラ設定方法まとめ](https://dev.classmethod.jp/articles/opencv-webcam-setting/)
+```
+$ sudo apt install v4l-utils
+$ v4l2-ctl --list-formats-ext
+```
+### OpenCVでのカメラの設定
+```c = cv2.VideoCapture (-1)
+c.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+c.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('Y','U','Y','V'))
+c.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+c.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+c.set(cv2.CAP_PROP_FPS, 15)
+c.set(cv.CAP_PROP_BUFFERSIZE,1)
+```
+### <カメラ関連参考資料>
+
+> [サイズ変更・回転・切り抜き](https://axa.biopapyrus.jp/ia/opencv/image-crop.html)
+>
+> [Basics of OpenCV (Resizing, Cropping, Rotation, and some other image Attributes)](https://www.codespeedy.com/basics-of-opencv-resizing-cropping-rotation/)
+>
+> [YUY2とYUYVについて](https://www.argocorp.com/UVC_camera/Linux_YUY2_YUYV.html)
+> 
+> [TISカメラをPythonで動作させる方法](https://www.argocorp.com/UVC_camera/Linux_TIScam_Python.html)
+>
+> [LinuxでUVC(USB Video Class)タイプのUSBカメラを使う](https://qiita.com/nonbiri15/items/9593d61a2be81f2b31a9)
+>
+> [Raspberry Piに接続されているwebカメラの情報をv4l2-ctlで簡単に取得したい](https://qiita.com/nonbiri15/items/9593d61a2be81f2b31a9)
+>
---
+++ airrc/image_pro.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# by T.Nishimura
+# 2020.10.22
+#
+import cv2
+import numpy as np
+import time
+import math 
+#
+def read_img():
+  img = cv2.imread('image.png')
+  return (img)
+#
+def write_img(nameimg,img):
+  cv2.imwrite(nameimg,img)
+#
+def resize_img(img,w,h):
+  ##resize--
+  img = cv2.resize(img,(w,h))
+  return (img)
+#
+def zoom_img(img,zo):#zo=0,1,2,3,4
+  h,w,c=img_size(img)
+  #trim
+  img_zoom = img[int(h*zo/10):int(h-h*zo/10), int(w*zo/10):int(w-w*zo/10)]
+  return (img_zoom)
+#
+def rotat(img,ang):
+  h,w,c=img_size(img)
+  m = cv2.getRotationMatrix2D((w / 2, h / 2), ang, 1)
+  im_roto = cv2.warpAffine(img, m, (w,h))
+  return(im_roto)
+  #
+def edge_pro(img):
+  ##edge--
+  gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+  gray = cv2.GaussianBlur(gray, (5,5), 1)
+  #cv2.imshow('frame0',gray)
+  edges = cv2.Canny(gray,3,20,apertureSize = 3)
+  kernel = np.ones((3,3),np.uint8)
+  #kernel = np.ones((2,2),np.uint8)
+  edge = cv2.dilate(edges,kernel,iterations = 1)
+  #cv2.imwrite('edges.jpg',edges)
+  return(edge)
+#
+def line_pro(img,edge):
+  ##line
+  #rhoは直線までの距離, thetaは回転角度
+  #threshold は直線を動かして、その直線に乗ってきた点の数
+  minLineLength = 150
+  maxLineGap = 40
+  lines = cv2.HoughLinesP(edge,1,np.pi/180,30,minLineLength,maxLineGap)
+  h,w,color=img.shape
+  #dislay
+  if lines is not None:
+    for line in lines:
+      x1,y1,x2,y2=line[0]
+      cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+  return(img,edge)
+#
+def circle_pro(img,edge):
+#circle dp 解像度2で1/2 minDist 円間距離 param1 小さいとエッジ検出 
+#   param2　小さいといろいろ検出 minRadius,maxRadius
+  minR=30
+  maxR=40
+  circles = cv2.HoughCircles(edge,cv2.HOUGH_GRADIENT,
+            1,100,param1=1,param2=10,minRadius=minR,maxRadius=maxR)
+  #print ('****** min=',minR,'max=',maxR,'circles=',circles)
+  if circles is not None:
+    # convert (x, y) coordinates and radius of the circles to integers
+    circles = np.round(circles[0, :]).astype("int")
+    for (x, y, r) in circles:
+      cv2.circle(img, (x, y), r, (0, 255, 0), 2)
+  return(img,edge)
+#
+def draw_circle(img,x,y,r):
+  cv2.circle(img, (x, y), r, (0, 255, 0), 2)
+#
+def imshow_pro(edge,img):
+  edge2 = cv2.cvtColor(edge,cv2.COLOR_GRAY2BGR)
+  img2=cv2.hconcat([edge2,img])
+  put_airrc(img2)
+  cv2.imshow('edge',img2)
+#
+def hcon_img(img1,img2):
+  img2=cv2.hconcat([edge2,img])
+  return(img)
+#
+def vcon_img(img1,img2):
+  img2=cv2.vconcat([edge2,img])
+  return(img)
+#
+def graytorgb(img):
+  img2 = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+  return(img2)
+#
+def put_airrc(img):
+  cv2.putText(img, "AiRRC", (10,55),0, 1, (0, 0, 255), 1, cv2.LINE_AA)
+  return(img)
+#
+def img_size(img):
+  height, width, channels = img.shape[:3]
+  return height, width, channels
+#--------------------------------------------------
+def main():
+  print('image processing in python-opencv')
+  cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('Y','U','Y','V'))
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+  cap.set(cv2.CAP_PROP_FPS, 15)
+  cap.set(cv2.CAP_PROP_BUFFERSIZE,1)
+  while(cap.isOpened()):
+    #for x in range(0,2):
+    ret, img = cap.read()
+    if ret==True:
+      img=rotat(img,10)
+      img=zoom_img(img,3)
+      img=resize_img(img,160,120)
+      cv2.imshow('image',img)
+      #edge=edge_pro(img)
+      #img,edge1=line_pro(img,edge)
+      #img,edge2=circle_pro(img,edge)
+      #imshow_pro(edge1,img)
+      #imshow_pro(edge2,img)
+      #time.sleep(0.5)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        print ("quit")
+        break
+    else:
+        print ("no flame")
+        break
+  cap.release()
+  cv2.destroyAllWindows()
+#--------------------------------------------------
+if __name__ == "__main__":
+  main()
